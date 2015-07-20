50094fa7889c79cf453b8310135abcf82e15ea8e
lilybader <baderlily@gmail.com>
2015-07-19 22:58:48 -0700

    Added Start/Stop

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -6,6 +6,31 @@ app.config(['$locationProvider', '$stateProvider', function($locationProvider, $
     $stateProvider
       .state('home', {
         url: '/',
+        controller: 'timerController',
         templateUrl: "templates/home.html"
       });
   }]);
+
+app.controller('timerController',['$scope', '$interval', function($scope, $interval){
+
+  $scope.timeLeft = 25;  
+  $scope.timeStart = function(){
+  $scope.disabled = true;
+    time = $interval(function(){
+      $scope.timeLeft--;
+      if($scope.timeLeft === 0){
+        $interval.cancel(time);
+      }
+    }, 1500 );
+  }  
+  
+  $scope.stopTimer = function(){
+    $scope.disabled = false;
+    $interval.cancel(time);
+  }
+  $scope.resetTimer = function(){
+    $scope.disabled = false;
+    $scope.timeLeft = 25;
+  }
+  
+ }]);
