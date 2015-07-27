0069421b7d25de82a316b60fe121d6bcb5c47a74
lilybader <baderlily@gmail.com>
2015-07-27 15:14:21 -0700

    added 5-min break

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -12,25 +12,75 @@ app.config(['$locationProvider', '$stateProvider', function($locationProvider, $
   }]);
 
 app.controller('timerController',['$scope', '$interval', function($scope, $interval){
+  	$scope.timeLeft = 1500;
 
-  $scope.timeLeft = 25;  
-  $scope.timeStart = function(){
-  $scope.disabled = true;
-    time = $interval(function(){
-      $scope.timeLeft--;
-      if($scope.timeLeft === 0){
-        $interval.cancel(time);
-      }
-    }, 1500 );
-  }  
+  	$scope.timeStart = function(){
+  		$scope.disabled = true;
+
+    	time = $interval(function(){
+      		$scope.timeLeft--;
+      		if($scope.timeLeft === 0){
+        		$interval.cancel(time);
+            $scope.breakTime = 300;
+      		}
+    	}, 1000 );
+  	}  
   
-  $scope.stopTimer = function(){
-    $scope.disabled = false;
-    $interval.cancel(time);
-  }
-  $scope.resetTimer = function(){
-    $scope.disabled = false;
-    $scope.timeLeft = 25;
-  }
+  	$scope.stopTimer = function(){
+    	$scope.disabled = false;
+    	$interval.cancel(time);
+  	}
+  	$scope.resetTimer = function(){
+    	$scope.disabled = false;
+    	$scope.timeLeft = 1500;
+  	}
   
+
+    $scope.breakStart = function() {
+      $scope.breakTime = 300;
+      $scope.disabled = true; 
+
+      brk = $interval(function(){
+          $scope.breakTime--;
+          if($scope.breakTime === 0){
+            $interval.cancel(brk);
+            $scope.breakTime = undefined;
+          }
+      }, 1000 );
+    }  
+
+    $scope.stopBreak = function(){
+      $scope.disabled = false;
+      $interval.cancel(brk);
+      $scope.breakTime = undefined;
+    }
+
+    $scope.resetBreak = function(){
+      $scope.disabled = false;
+      $scope.timeLeft = 300;
+    }
+    
+    $scope.beginWork = function() {
+      $scope.breakTime = undefined; 
+
+    }
+
+    $scope.beginBreak = function() {
+      $scope.breakTime = 300;
+    }
+
  }]);
+
+app.filter('convertToCountdown', function() {
+	return function(ms) {
+		var minutes = parseInt(ms/60, 10); //25
+		var seconds = parseInt(ms%60, 10); //0
+    
+    if (seconds < 10) {
+      seconds = '0' + seconds 
+    }
+
+		return minutes + ':' + seconds;
+
+	};
+});
