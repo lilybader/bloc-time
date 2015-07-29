53627d0cef472cc7df616692c0e48998425bb412
lilybader <baderlily@gmail.com>
2015-07-29 10:15:56 -0700

    long break commented out

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -11,19 +11,28 @@ app.config(['$locationProvider', '$stateProvider', function($locationProvider, $
       });
   }]);
 
-app.controller('timerController',['$scope', '$interval', function($scope, $interval){
-  	$scope.timeLeft = 1500;
+app.controller('timerController',['$scope', '$interval', '$firebase' function($scope, $interval, $firebase){
+    var ref = new Firebase('https://bloc-time.firebaseio.com/time');
+    var track = $firebase(ref);
+
+    $scope.timeLeft = 120; //change back to 1500 after tests 
 
   	$scope.timeStart = function(){
   		$scope.disabled = true;
 
+// Do I need to change the state everytime it finishes?
     	time = $interval(function(){
       		$scope.timeLeft--;
       		if($scope.timeLeft === 0){
         		$interval.cancel(time);
             $scope.breakTime = 300;
+            // var times = times.$getRecord(timeId);
+            // time.state = "complete";
+            // times.$save(time);
       		}
     	}, 1000 );
+      // if statement?
+
   	}  
   
   	$scope.stopTimer = function(){
@@ -32,7 +41,7 @@ app.controller('timerController',['$scope', '$interval', function($scope, $inter
   	}
   	$scope.resetTimer = function(){
     	$scope.disabled = false;
-    	$scope.timeLeft = 1500;
+    	$scope.timeLeft = 120; //change back to 1500 after tests
   	}
   
 
@@ -69,6 +78,35 @@ app.controller('timerController',['$scope', '$interval', function($scope, $inter
       $scope.breakTime = 300;
     }
 
+    // $scope.completeWork = function(){
+    //   $scope.longBreak = function(){
+    //     $scope.longTime = 1800;
+    //     $scope.disabled = true;
+
+    //     longBrk = $interval(function(){
+    //       $scope.longTime--;
+    //       if($scope.longTime === 0){
+    //         $interval.cancel(longBrk);
+    //         $scope.longTime = undefined;
+    //       }
+    //   }, 1000 );
+
+    // $scope.stopLongBreak = function(){
+    //   $scope.disabled = false;
+    //   $interval.cancel(longBrk);
+    //   $scope.longTime = undefined;
+    // }
+
+    // $scope.resetLongBreak = function(){
+    //   $scope.disabled = false;
+    //   $scope.timeLeft = 300;
+    // }
+    //   }
+
+      // if (i === 4) 
+      //   return $scope.completeWork; 
+    
+    }
  }]);
 
 app.filter('convertToCountdown', function() {
